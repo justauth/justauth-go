4b9b34012c543af94a67abb3acd3161e7d09e3c1
zhuojian <zhuojian@cmcm.com>
2022-02-08 21:07:30 +0800

    feat:抽象类实现

--- justauth_test.go
+++ justauth_test.go
@@ -1,10 +1,17 @@
 package justauth
 
-import "testing"
+import (
+	"github.com/justauth/justauth-go/request"
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestHello(t *testing.T) {
 	want := "Hello, justauth."
 
 	assert.Equal(t, want, Hello())
+
+	authWxRequest := &request.AuthWxRequest{}
+
+	authWxRequest.Play(authWxRequest)
 }
