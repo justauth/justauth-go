3fab118f74f92c3ee7b5f5eda1c2a311695fb351
zhuojian <zhuojian@cmcm.com>
2022-02-12 15:02:09 +0800

    feat:Gietee

--- model/biz_objects.go
+++ model/biz_objects.go
@@ -10,8 +10,9 @@ type AuthCallback struct {
 }
 
 type AuthResponse struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
 
 type AuthToken struct {
@@ -23,4 +24,39 @@ type AuthToken struct {
 	OpenId               string `json:"openId"`
 	AccessCode           string `json:"accessCode"`
 	UnionId              string `json:"unionId"`
+}
+
+type AuthConfig struct {
+	ClientId               string `json:"clientId"`
+	ClientSecret           string `json:"client_secret"`
+	RedirectUri            string `json:"redirect_uri"`
+	AlipayPublicKey        string `json:"alipay_public_key"`
+	UnionId                bool   `json:"union_id"`
+	StackOverflowKey       string `json:"stackoverflow_key"`
+	AgentId                string `json:"agent_id"`
+	DomainPrefix           string `json:"domain_prefix"`
+	IgnoreCheckState       bool   `json:"ignore_check_state"`
+	Scopes                 string `json:"scopes"`
+	DeviceId               string `json:"device_id"`
+	ClientOsType           int    `json:"client_os_type"`
+	PackId                 string `json:"pack_id"`
+	Pkce                   bool   `json:"pkce"`
+	AuthServerId           string `json:"auth_server_id"`
+	IgnoreCheckRedirectUri bool   `json:"ignore_check_redirect_uri"`
+}
+
+type AuthUser struct {
+	Uuid        string    `json:"uuid"`
+	Username    string    `json:"username"`
+	Nickname    string    `json:"nickname"`
+	Avatar      string    `json:"avatar"`
+	Blog        string    `json:"blog"`
+	Company     string    `json:"company"`
+	Location    string    `json:"location"`
+	Email       string    `json:"email"`
+	Remark      string    `json:"remark"`
+	Gender      string    `json:"gender"`
+	Source      string    `json:"source"`
+	AuthToken   AuthToken `json:"clientId"`
+	RawUserInfo string    `json:"rawUserInfo"`
 }
--- request/AuthDefaultRequest.go
+++ request/AuthDefaultRequest.go
@@ -1,23 +1,40 @@
 package request
 
 import (
+	"fmt"
 	"github.com/justauth/justauth-go/model"
 )
 
-type AuthDefaultRequest struct{}
-
-func (d *AuthDefaultRequest) Authorize(state string) string {
-	return "Authorize"
+type AuthDefaultRequest struct {
+	authConfig model.AuthConfig
 }
 
-func (d *AuthDefaultRequest) Login(authCallBack model.AuthCallback) model.AuthResponse {
+func (adq *AuthDefaultRequest) Authorize(state string) string {
+	return fmt.Sprintf("%s")
+}
+
+func (adq *AuthDefaultRequest) Login(authCallBack model.AuthCallback) model.AuthResponse {
+	authToken := adq.getAccessToken(authCallBack)
+	userInfo := adq.getUserInfo(authToken)
+	return model.AuthResponse{
+		Code: 200,
+		Msg:  "",
+		Data: userInfo,
+	}
+}
+
+func (adq *AuthDefaultRequest) Revoke(authToken model.AuthToken) model.AuthResponse {
 	return model.AuthResponse{}
 }
 
-func (d *AuthDefaultRequest) Revoke(authToken model.AuthToken) model.AuthResponse {
+func (adq *AuthDefaultRequest) Refresh(authToken model.AuthToken) model.AuthResponse {
 	return model.AuthResponse{}
 }
 
-func (d *AuthDefaultRequest) Refresh(authToken model.AuthToken) model.AuthResponse {
-	return model.AuthResponse{}
+func (adq *AuthDefaultRequest) getAccessToken(authCallback model.AuthCallback) model.AuthToken {
+	return model.AuthToken{}
+}
+
+func (adq *AuthDefaultRequest) getUserInfo(authToken model.AuthToken) model.AuthUser {
+	return model.AuthUser{}
 }
